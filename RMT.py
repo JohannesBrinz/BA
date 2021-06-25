1a6f04a08dc3734e7a5f9a10bf128d8feeb5ca95
Schwannes <johannesbrinz@web.de>
2021-06-25 18:42:59 +0200

    version_25.6.

--- RMT.py
+++ RMT.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 #Defining important constants
 
-N = int(30)
-M = int(100)
+N = int(10)
+M = int(5000)
 
 
 #Defining matricies
@@ -19,6 +19,7 @@ U = np.zeros(shape=(N*N,N*N))
 tildekappa = np.zeros(shape=(N*N,N*N))
 K_0 = np.zeros(shape=(N*N,N*N))
 Lambda = np.array([])
+dif = np.array([])
 
 
 #Defining S and \mathcalS
@@ -129,12 +130,27 @@ for e in range(M):
 
 
 print(Lambda, len(Lambda))
+Lambda_sort = np.sort(Lambda)
+
+for i in range(len(Lambda_sort)-1):
+     dif = np.append(dif, Lambda_sort[i+1]-Lambda_sort[i])
+
+print("Das ist die Differenz:   ", dif)
 
 #plotting histogram
-n, bins, patches = plt.hist(Lambda, bins = 10000, range = (-1, 1), density = True)
+n, bins, patches = plt.hist(Lambda, bins = 1000, range = (-1, 10), density = True)
 
-plt.title('Histogram eigen values', fontsize = 15)
+plt.title('Histogram eigenvalues', fontsize = 15)
 plt.xlabel('$\lambda$', fontsize = 13)
 plt.ylabel('probability distribution', fontsize = 13)
 plt.savefig('Plots/Hist.png', dpi=300)
+plt.clf()
+
+#distance
+n, bins, patches = plt.hist(dif, bins = 1000, range = (0, 0.01), density = True)
+
+plt.title('Histogram correlation', fontsize = 15)
+plt.xlabel('$\lambda_i - \lambda_j$', fontsize = 13)
+plt.ylabel('probability distribution', fontsize = 13)
+plt.savefig('Plots/Hist_dist.png', dpi=300)
 plt.clf()
